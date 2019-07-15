3ff5dc3706a5626c908cf275e75f8206ea02d4f0
ZijiaZhang <zijia.zhang99@gmail.com>
2019-07-14 20:37:06 -0700

    Add First Runner Block.

---
+++ Source/SciFiRunner/RunnerBloak_Normal.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RunnerBlock.h"
+#include "RunnerBloak_Normal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCIFIRUNNER_API ARunnerBloak_Normal : public ARunnerBlock
+{
+	GENERATED_BODY()
+	
+};
