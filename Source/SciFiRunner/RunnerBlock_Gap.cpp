4884b74bd76c5dacd5bfba7079f5f538928b8fc5
ZijiaZhang <zijia.zhang99@gmail.com>
2019-07-29 16:30:28 -0700

    Add Runner Block Gap.

---
+++ Source/SciFiRunner/RunnerBlock_Gap.cpp
@@ -0,0 +1,25 @@
+﻿//COPYRIGHT 2019 Zijia Zhang All Rights Reserved
+
+
+#include "RunnerBlock_Gap.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+ARunnerBlock_Gap::ARunnerBlock_Gap() {
+	Arrow->SetRelativeLocation(FVector(256*3, 0, 0));
+
+	spawnTrigger->SetBoxExtent(FVector(20, 128, 128));
+	spawnTrigger->SetRelativeLocation(FVector(256*3, 0, 64));
+
+	Floor1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("floor1"));
+	Floor1->SetStaticMesh(LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/EngineMeshes/Cube")));
+	Floor1->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+	Floor1->SetRelativeLocation(FVector(128.0f, 0, 0));
+	Floor1->SetRelativeScale3D(FVector(1.0f, 1.0f, 0.2f));
+
+	Floor2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("floor2"));
+	Floor2->SetStaticMesh(LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/EngineMeshes/Cube")));
+	Floor2->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+	Floor2->SetRelativeLocation(FVector(128.0f + 256*2, 0, 0));
+	Floor2->SetRelativeScale3D(FVector(1.0f, 1.0f, 0.2f));
+}
---
+++ Source/SciFiRunner/RunnerBlock_Gap.h
@@ -0,0 +1,22 @@
+﻿// COPYRIGHT 2019 Zijia Zhang All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RunnerBlock.h"
+#include "RunnerBlock_Gap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCIFIRUNNER_API ARunnerBlock_Gap : public ARunnerBlock
+{
+	GENERATED_BODY()
+	public:
+		ARunnerBlock_Gap();
+		UPROPERTY(AdvancedDisplay, EditAnywhere)
+			UStaticMeshComponent* Floor1;
+		UPROPERTY(AdvancedDisplay, EditAnywhere)
+			UStaticMeshComponent* Floor2;
+};
