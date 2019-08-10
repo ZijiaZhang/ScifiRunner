a64e8726fa579177821a5c6b66b62c311ee233ea
ZijiaZhang <zijia.zhang99@gmail.com>
2019-08-09 17:15:42 -0700

    Add Main Menu.

---
+++ Source/SciFiRunner/MainMenu.h
@@ -0,0 +1,17 @@
+// COPYRIGHT 2019 Zijia Zhang All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCIFIRUNNER_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
